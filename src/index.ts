sanity-io/icons

refactor: restructure workspace

src/index.ts
@@ -1,0 +1,3 @@
+export * from './icon'
+export * from './icons'
+export * from './types'

sanity-io/logos

src/index.ts
@@ -1,0 +1,4 @@
+export * from './groqLogo'
+export * from './groqMonogram'
+export * from './sanityLogo'
+export * from './sanityMonogram'